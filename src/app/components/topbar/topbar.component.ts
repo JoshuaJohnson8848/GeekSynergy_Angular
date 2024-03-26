5d30d4d68a29ba0209690d0a4e561300145300ca
JoshuaJohnson8848 <joshuatjohnson255@gmail.com>
2024-03-26 08:18:34 +0530

    Logout Toastr

--- src/app/components/topbar/topbar.component.ts
+++ src/app/components/topbar/topbar.component.ts
@@ -1,18 +1,20 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { ToastrModule, ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-topbar',
   standalone: true,
-  imports: [],
+  imports: [ToastrModule],
   templateUrl: './topbar.component.html',
   styleUrl: './topbar.component.scss'
 })
 export class TopbarComponent {
-  constructor(private _router: Router){}
+  constructor(private _router: Router, private _toastr: ToastrService){}
 
   logout(){
     localStorage.clear();
     this._router.navigate(['/']);
+    this._toastr.success('Logged Out','Successfully')
   }
 }
